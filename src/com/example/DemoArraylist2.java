24b43fe3baffcd845b80e55e677ffd97ecfa6595
Jerry Johansson <info@jerryjohansson.com>
2020-09-18 10:54:42 +0200

    Ändrat kommentar för meny

--- src/com/example/DemoArraylist2.java
+++ src/com/example/DemoArraylist2.java
@@ -22,7 +22,7 @@ public class DemoArraylist2 {
     boolean quit = false;
     startPhone();
     printActions();
-    /* Vår meny */
+    /* Vår meny i en while-loop */
     while(!quit) {
       System.out.println("\nVälj: (6 för att visa val)");
       int action = scanner.nextInt();
